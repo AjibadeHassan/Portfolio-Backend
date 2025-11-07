8fdbb77f64469c9e5b023602e45d00878eb00112
Hassan <hassanajibade17@gmail.com>
2025-11-07 16:10:47 +0100

    modified urls to match app name

--- Contact/urls.py
+++ Contact/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import contact_message
 
 urlpatterns = [
-    path("api/contact/", contact_message, name="contact_message"),
+    path("contact/", contact_message, name="contact"),
 ]
--- core/urls.py
+++ core/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('/', include('Contact.urls')),
     path('admin/', admin.site.urls),
-    path('api/contact/', include('Contact.urls')),
+    path('api/', include('Contact.urls')),
 ]
